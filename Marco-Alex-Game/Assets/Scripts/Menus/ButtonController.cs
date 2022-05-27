4968c67773dcc61e53d3ed425cb476e1aa91b18b
sangiovanni-marco <77410198+sangiovanni-marco@users.noreply.github.com>
2022-05-27 17:20:48 -0400

    Main Menu Scene
    + added Start Button
    + added Exit Button
    + added Settings Button
    + added Settings Menu
    + added Music Slider (Still need to code it)
    + added Sound Slider (Still need to code it)
    +added Back Button

---
+++ Marco-Alex-Game/Assets/Scripts/Menus/ButtonController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonController : MonoBehaviour, IPointerEnterHandler, IDeselectHandler
+{
+    public void OnDeselect(BaseEventData eventData)
+    {
+        GetComponent<Selectable>().OnPointerExit(null);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        GetComponent<Selectable>().Select();
+    }
+}
